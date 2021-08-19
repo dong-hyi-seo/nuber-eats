143853a9de7e09684ad5208f870512f69040d8d4
dhseo <dhseo@antock.com>
2021-08-19 20:43:31 +0900

    1. userProfile, me testing code 까지 완료

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -18,8 +18,10 @@ export class JwtMiddleware implements NestMiddleware {
       try {
         const decoded = this.jwtService.verify(token.toString());
         if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
-          const { user } = await this.userService.findById(decoded['id']);
-          req['user'] = user; //request apollo server context 공유가가능
+          const { user, ok } = await this.userService.findById(decoded['id']);
+          if (ok) {
+            req['user'] = user; //request apollo server context 공유가가능
+          }
         }
       } catch (e) {
         Logger.error(e);
--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 import { getConnection, Repository } from 'typeorm';
 import { User } from '../src/users/entities/user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
@@ -15,11 +15,11 @@ const GRAPHQL_ENDPOINT = '/graphql';
 const testUser = {
   email: 'nico@las.com',
   password: '12345',
-}
+};
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
-  let userRepository: Repository<User>
+  let userRepository: Repository<User>;
   let jwtToken: string;
 
   beforeAll(async () => {
@@ -28,7 +28,7 @@ describe('UserModule (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
-    userRepository = module.get<Repository<User>>(getRepositoryToken(User))
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
     await app.init();
   });
 
@@ -89,11 +89,11 @@ describe('UserModule (e2e)', () => {
   });
 
   describe('login', () => {
-    it("should login with correct", () => {
+    it('should login with correct', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
         .send({
-          query :`
+          query: `
             mutation {
               login(input: {
                 email: "${testUser.email}",
@@ -104,26 +104,26 @@ describe('UserModule (e2e)', () => {
                 token
               }
             }
-          `
+          `,
         })
         .expect(200)
-        .expect(res => {
+        .expect((res) => {
           const {
-            body : {
-              data : { login },
-            }
+            body: {
+              data: { login },
+            },
           } = res;
           expect(login.ok).toBe(true);
           expect(login.error).toBe(null);
           expect(login.token).toEqual(expect.any(String));
           jwtToken = login.token;
-        })
-    })
-    it("should not be able to login with wrong credentials", () => {
+        });
+    });
+    it('should not be able to login with wrong credentials', () => {
       return request(app.getHttpServer())
         .post(GRAPHQL_ENDPOINT)
         .send({
-          query :`
+          query: `
             mutation {
               login(input: {
                 email: "${testUser.email}",
@@ -134,35 +134,148 @@ describe('UserModule (e2e)', () => {
                 token
               }
             }
-          `
+          `,
         })
         .expect(200)
-        .expect(res => {
+        .expect((res) => {
           const {
-            body : {
-              data : { login },
-            }
+            body: {
+              data: { login },
+            },
           } = res;
           expect(login.ok).toBe(false);
           expect(login.error).toBe('Wrong password');
           expect(login.token).toBe(null);
-        })
-    })
-  })
+        });
+    });
+  });
 
   describe('userProfile', () => {
-    let userId : number;
+    let userId: number;
     beforeAll(async () => {
-      console.log(await userRepository.find())
       const [user] = await userRepository.find();
-    })
+      userId = user.id;
+    });
     it("should see a user's profile", () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+            {
+            userProfile(userId:${userId}) {
+                ok
+                error
+                user {
+                  id
+                }
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                userProfile: {
+                  ok,
+                  error,
+                  user: { id },
+                },
+              },
+            },
+          } = res;
+          expect(ok).toBe(true);
+          expect(error).toBe(null);
+          expect(id).toBe(userId);
+        });
+    });
+    it('should not find a profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+            {
+            userProfile(userId:6) {
+                ok
+                error
+                user {
+                  id
+                }
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                userProfile: { ok, error, user },
+              },
+            },
+          } = res;
+          expect(ok).toBe(false);
+          expect(error).toBe('User Not Found');
+          expect(user).toBe(null);
+        });
+    });
+  });
 
-    })
-    it.todo("should not find a profile");
-  })
-  it.todo('userProfile');
-  it.todo('me');
+  describe('me', () => {
+    it('should find my profile', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .set('X-JWT', jwtToken)
+        .send({
+          query: `
+            {
+              me {
+                email
+              }
+            }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: {
+                me: { email },
+              },
+            },
+          } = res;
+          expect(email).toBe(testUser.email);
+        });
+    });
+    it('should not allow logged out user', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            {
+            userProfile(userId:6) {
+                ok
+                error
+                user {
+                  id
+                }
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: { errors },
+          } = res;
+          const [error] = errors;
+          expect(error.message).toBe('Forbidden resource');
+        });
+    });
+  });
   it.todo('verifyEmail');
   it.todo('editProfile');
 });
