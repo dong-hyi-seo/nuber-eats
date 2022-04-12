dd2574c0d50346c29300e27cfe4f661585175aa4
dhseo <dhseo@antock.com>
2022-04-12 19:55:46 +0900

    1. 첫 router 적용 2. login 페이지 만드는중

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Login = () => {
+  console.log('is login!');
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-screen-sm">
+        <h3 className="font-bold text-lg text-gray-800">Log In</h3>
+      </div>
+    </div>
+  );
+};
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,62 +1,18 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-//useForm 과 typescript 통합
-interface IForm {
-  email: string;
-  password: string;
-}
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Login } from '../pages/login';
+import { CreateAccount } from '../pages/create-account';
+
+//browser router example : google.com/hello
+//hash router example : google.com/#hello
+//강의에서 차이점 react-router-dom v6 부터 Switch component가 Routes로 변경됨
 export const LoggedOutRouter = () => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>();
-  //register 는 input에서 받은값을 지정하고
-  //watch는 register에 입력될때마다 값을 받음
-
-  //react form 제출 함수
-  const onSubmit = () => {
-    console.log(watch('email'));
-  };
-
-  //login valid!!
-  const onInvalid = () => {
-    console.log('cant login account');
-  };
-  console.log('errors = ', errors.email?.message);
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            {...register('email', {
-              required: 'This is required',
-              pattern: /^[A-Za-z0-9._%+-]+@gmail.com%/,
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className="font-bold text-red-600">
-              {errors.email?.message}
-            </span>
-          )}
-          {errors.email?.type === 'pattern'}
-        </div>
-        <div>
-          <input
-            {...register('password', { required: true })}
-            name="password"
-            type="password"
-            required
-            placeholder="password"
-          />
-        </div>
-        <button className="bg-yellow-300">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 };
